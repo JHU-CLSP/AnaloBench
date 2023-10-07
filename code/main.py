a5974173ec65d69a623e9e93a431b9852b80a5a6
Daniel Khashabi <danielk@jhu.edu>
2023-10-06 21:15:33 -0400

    minor.

--- code/main.py
+++ code/main.py
@@ -198,7 +198,11 @@ if __name__ == '__main__':
                     if sent1 not in processed_stories:
                         processed_stories[sent1] = prompts.story_names(names1, row["Story1"])
                     story1 = processed_stories[sent1]
-                    story2 = prompts.story_names(names2, row["Story2"])
+
+                    sent2 = row["Sentence2"]
+                    if sent2 not in processed_stories:
+                        processed_stories[sent2] = prompts.story_names(names2, row["Story2"])
+                    story2 = processed_stories[sent2]
                 except Exception as e:
                     print(f"An error occurred: {e}")
                     raise
